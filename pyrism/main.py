dcf02b794780afbee68486720b6d0119f3c5cab4
SahayanKY <a1c4f0x324@gmail.com>
2023-11-29 00:52:07 +0900

    feat: フーリエ変換と反復計算の部分の処理を追加

--- pyrism/main.py
+++ pyrism/main.py
@@ -7,9 +7,31 @@ from scipy.linalg import block_diag
 from scipy.fft import dst
 
 
+def fft3d_spsymm(r, dr, k, dk, f):
+    """
+    f: shape: (numgrid, *, *)
+    """
+    t_f = 4*np.pi/k[:,np.newaxis,np.newaxis] * dst(r[:,np.newaxis,np.newaxis] * f, type=1, axis=0) / 2 * dr
+    return t_f
+
+def ifft3d_spsymm(r, dr, k, dk, t_f):
+    """
+    t_f: shape: (numgrid, *, *)
+    """
+    f = 1/(2*np.pi**2 * r[:,np.newaxis,np.newaxis]) * dst(k[:,np.newaxis,np.newaxis] * t_f, type=1, axis=0) / 2 * dk
+    return f
+
+
 # インプット読み込み
 inputFile = 'sampleinput.json'
 jsonDict = json.load(open(inputFile, 'r'))
+
+
+# 収束パラメータ
+mixingParam = jsonDict['config']['mixingParam']
+chargeUp = jsonDict['config']['chargeUp']
+criteria = jsonDict['config']['converge']
+maxIterNum = jsonDict['config']['maxiter']
 
 # 温度情報
 temperature = jsonDict['temperature'] # 温度: K
@@ -55,25 +77,57 @@ t_W = bI * np.sinc(k[:,np.newaxis,np.newaxis] * L) # shape: (numgrid,totalN,tota
 
 # サイト間短距離ポテンシャル行列: [無次元]: shape: (numgrid, totalN, totalN)
 # Lorentz-Berthelot則
-Sigma = (sigma[:,np.newaxis] + sigma) / 2 # shape: (totalN,totalN) # LJ sigma_ij
-Eps = np.sqrt(eps[:,np.newaxis]*eps)      # shape: (totalN,totalN) # LJ eps_ij
-LJA = 4 * Eps * Sigma**12 # shape: (totalN,totalN) # LJ A係数: A^12
-LJB = 4 * Eps * Sigma**6  # shape: (totalN,totalN) # LJ B係数: A^6
-Us = beta * (LJA / (r[:,np.newaxis,np.newaxis]**12) - LJB / (r[:,np.newaxis,np.newaxis]**6))
-
+Sigma = (sigma[:,np.newaxis] + sigma) / 2           # shape: (totalN,totalN) # LJ sigma_ij
+Eps = np.sqrt(eps[:,np.newaxis]*eps)                # shape: (totalN,totalN) # LJ eps_ij
+__sigmar6 = (Sigma / r[:,np.newaxis,np.newaxis])**6 # shape: (numgrid, totalN, totalN)
+Us = beta * 4 * Eps * (__sigmar6**2 - __sigmar6)
 # サイト間長距離ポテンシャル行列: [無次元]: shape: (numgrid, totalN, totalN)
 Ul = beta * 332.053 * z[:,np.newaxis] * z / r[:,np.newaxis,np.newaxis]
 
 # サイト間長距離直接相関行列: shape: (numgrid, totalN, totalN)
-#(実空間): [無次元]
+# (実空間): [無次元]
 Cl = Ul
-#(波数空間): A^3
+# (波数空間): A^3
 t_Cl = -beta * 332.053 * z[:,np.newaxis] * z * 4*np.pi / (k[:,np.newaxis,np.newaxis]**2)
 # サイト間長距離全相関行列  : shape: (numgrid, totalN, totalN)
-#(波数空間): A^3
-t_Hl = t_W @ t_Cl @ t_W @ np.linalg.inv(I-P@t_Cl@t_W)
-#(実空間): [無次元]
-# フーリエ変換
+# (波数空間): A^3
+t_Hl = t_W @ t_Cl @ t_W @ np.linalg.inv(I - P @ t_Cl @ t_W)
+# (実空間): [無次元]: フーリエ逆変換
+Hl = ifft3d_spsymm(r, dr, k, dk, t_Hl)
+
+# Hypervertex
+t_Omega = t_W + P@t_Hl
+t_OmegaT = t_Omega.transpose(0,2,1)
+
+# initialize
+# 間接相関 Hs - Cs: shape: (numgrid, totalN, totalN)
+Eta = np.zeros(Cl.shape)
+
+numLoop = 0
+while True:
+    numLoop += 1
+    if numLoop > maxIterNum:
+        break
+
+    # サイト間短距離直接相関行列: shape: (numgrid, totalN, totalN)
+    Cs = np.exp(-Us+Eta) -Eta -1 # HNC closure
+    # フーリエ変換
+    t_Cs = fft3d_spsymm(r, dr, k, dk, Cs)
+
+    # サイト間短距離全相関行列  : shape: (numgrid, totalN, totalN)
+    # (波数空間): A^3
+    t_Hs = t_OmegaT @ t_Cs @ t_Omega @ np.linalg.inv(I - P @ t_Cs @ t_Omega)
+    # (実空間): [無次元]
+    Hs = ifft3d_spsymm(r, dr, k, dk, t_Hs)
+
+    # 間接相関更新
+    newEta = Hs - Cs
+    # 収束判定
+    maxError = np.max(newEta - Eta)
+    if maxError < criteria:
+        break
+    Eta = mixingParam * newEta + (1-mixingParam) * Eta
+
 
 
 
