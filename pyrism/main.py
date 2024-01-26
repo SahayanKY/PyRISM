a8101a14e4002bbbcbc348b2b88c38e7fbdc838f
SahayanKY <a1c4f0x324@gmail.com>
2024-01-26 23:55:49 +0900

    feat: main実装

---
+++ pyrism/main.py
@@ -0,0 +1,21 @@
+from rism import RISM
+
+# python main.py input.json
+
+# インプット読み込み
+inputFile = sys.argv[1]
+jsonDict = json.load(open(inputFile, 'r'))
+
+temperature = jsonDict.get('temperature', 300)
+closure = jsonDict.get('closure', None)
+# closureに関しては後で実装方法を練り直す
+rismDict = jsonDict['1DRISM']
+
+# RISMオブジェクト生成
+rism = RISM(rismDict, temperature, closure)
+# 1D-RISM計算
+rism.solve()
+
+
+
+
